cab2a5741a715c7081666fccb042301654abdfaf
Ilyas Khairullin <ildar97.97@gmail.com>
2017-05-16 22:09:38 +0300

    travis

--- HWL3.py
+++ HWL3.py
@@ -1,14 +1,19 @@
+'''
+Module documentation
+'''
 import datetime
 
 
 class Task:
-
+    '''
+    Task class docstring
+    '''
     def __init__(self, title, estimate, state='in_progress'):
-        if type(title) != str:
+        if isinstance(title, str):
             raise TypeError('неверный формат заголовка')
-        elif type(estimate) != datetime.date:
+        elif isinstance(estimate, datetime.date):
             raise TypeError('неверный тип даты')
-        elif type(state) != str:
+        elif isinstance(state, str):
             raise TypeError('неверный формат состояния')
         elif state != 'in_progress' and state != 'ready':
             raise AttributeError('невозможное состояние задачи')
@@ -19,36 +24,35 @@ class Task:
 
     @property
     def title(self) -> str:
-        '''
-        
-        :return: tittle
-        :rtype: str
-        '''
+        '''function docstring'''
         return self._title
 
     @property
     def estimate(self) -> datetime.date:
+        '''function docstring'''
         return self._estimate
 
     @property
     def state(self) -> str:
+        '''function docstring'''
         return 'in_progress' if self._state else 'ready'
 
     @property
     def remaining(self) -> datetime.timedelta:
+        '''function docstring'''
         if self._state:
             return self._estimate - datetime.date.today()
-        else:
-            return datetime.timedelta(0)
+        return datetime.timedelta(0)
 
     @property
     def is_failed(self) -> bool:
+        '''function docstring'''
         if self._state and self.remaining < datetime.timedelta(0):
             return True
-        else:
-            return False
+        return False
 
     def ready_task(self):
+        '''function docstring'''
         self._state = False
 
 
@@ -59,19 +63,20 @@ class Roadmap:
     def __init__(self, tasks=None):
         if not tasks:
             self._tasks = []
-        elif type(tasks) == list and all(type(task) == Task for task in tasks):
+        elif isinstance(tasks, list) and all(isinstance(task, Task) for task in tasks):
             self._tasks = tasks
-        else:
-            raise TypeError
+        raise TypeError
 
     def add(self, task):
-        if type(task) != Task:
+        '''function docstring'''
+        if isinstance(task, Task):
             raise TypeError('ожидается элемент типа Task')
         else:
             self._tasks.append(task)
 
     def filter(self, state) -> list:
-        if type(state) != str:
+        '''function docstring'''
+        if isinstance(state, str):
             raise TypeError
         elif state != 'in_progress' and state != 'ready':
             raise AttributeError('невозможное состояние задачи')
@@ -83,10 +88,10 @@ class Roadmap:
             return returned_list
 
     def get_critical_tasks(self) -> list:
+        '''function docstring'''
         active_tasks = self.filter('in_progress')
         critical_tasks = []
         for task in active_tasks:
             if task.remaining <= datetime.timedelta(3):
                 critical_tasks.append(task)
         return critical_tasks
-
